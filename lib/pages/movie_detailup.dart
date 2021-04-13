1e262c92ede328e6522ec045a50514a25bd2833c
Yovieaji13 <71000479+Yovieaji13@users.noreply.github.com>
2021-04-13 14:33:21 +0700

    progress_finishing

--- lib/pages/movie_detailup.dart
+++ lib/pages/movie_detailup.dart
@@ -22,6 +22,8 @@ class MovieDetailUp extends StatelessWidget {
         title: Text(movie.title),
         backgroundColor: Colors.black,
       ),
+
+      backgroundColor: Colors.black,
       body: SingleChildScrollView(
         child: Center(
           child: Column(
@@ -31,15 +33,22 @@ class MovieDetailUp extends StatelessWidget {
                 height: height / 1.5,
                 child: Image.network(path),
               ),
-                
-                Padding(
+
+              Container(
+                child: Text('Sinopsis Movie ' + movie.title , style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)
+              ),
+
+                Container(
                  padding: EdgeInsets.all(20),
-                 child:  Card(
                   child: Text
                   (movie.overview, 
-                  style: TextStyle(color: Colors.black,fontSize: 15)),
-                  )
-                )
+                  style: TextStyle(color: Colors.white,fontSize: 15)),
+                  ),
+                  Container(
+                child: Text(' (Rating ' +  movie.voteAverage.toString() + ')', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),)
+              
+              ),
+                
             ],
           ),
         ),
